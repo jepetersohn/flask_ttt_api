447617427edc67fda51241fba6e90f58e167d6e0
alanahanson <jepetersohn@gmail.com>
2017-05-19 14:30:09 -0500

    got flask to return a new board state with its move in place

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import random
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/game", methods=['GET', 'POST'])
+@app.route("/game", methods=['GET'])
 def game():
-    return "almost there!"
+  boardState = list(request.args.getlist('board'))
+  boardArray = list(boardState[0])
+  move = randomPlay(boardArray)
+  newBoard = placeMove(move, boardArray)
+  return jsonify(newBoard)
+
+def randomPlay(board):
+  boardArray = [x for x in board if x != "X" and x != "O"]
+  return random.choice(boardArray)
+
+def placeMove(move, board):
+  board[int(move)] = "O"
+  return board
+
 
 if __name__ == "__main__":
     app.run(debug=True)
